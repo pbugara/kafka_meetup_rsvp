52871c250d5b2d42842928c0edd659ad8424391d
Piotr Bugara <piotr.bugara@gmail.com>
2019-01-30 17:43:40 +0100

    Project initialization. Collection Meetup RSVPs through Websocket

---
+++ src/main/java/pl/bugara/kafka/KafkaApplication.java
@@ -0,0 +1,32 @@
+package pl.bugara.kafka;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.socket.client.WebSocketClient;
+import org.springframework.web.socket.client.standard.StandardWebSocketClient;
+import pl.bugara.kafka.meetup.RsvpWebSocketHandler;
+
+@SpringBootApplication
+public class KafkaApplication {
+
+    @Value("${meetup.rsvp.url}")
+    private String meetupRsvpUrl;
+
+    public static void main(String[] args) {
+        SpringApplication.run(KafkaApplication.class, args);
+    }
+
+    @Bean
+    public ApplicationRunner initializeWebsocketConnectionWithMeetup(RsvpWebSocketHandler rsvpWebSocketHandler) {
+        return args -> {
+            WebSocketClient rsvpSocketClient = new StandardWebSocketClient();
+            rsvpSocketClient.doHandshake(
+                    rsvpWebSocketHandler, meetupRsvpUrl
+            );
+        };
+    }
+}
+
